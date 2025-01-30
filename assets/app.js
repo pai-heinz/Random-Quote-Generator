dfa72c9ab1f3d8132e03dac742bf5682c948db8e
phkyww <paingheink2003@gmail.com>
2025-01-30 11:35:01 +0630

    add text and author from random quote rest api

---
+++ assets/app.js
@@ -0,0 +1,19 @@
+let btn = document.querySelector(".btn");
+let quote_txt = document.querySelector(".quote-txt");
+let author = document.querySelector(".author");
+
+btn.addEventListener("click", () => {
+  btn.innerHTML = "Loading Quote..";
+  btn.classList.add("loading");
+  fetch("https://dummyjson.com/quotes/random")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      btn.classList.remove("loading");
+      btn.innerHTML = "New Quote";
+      quote_txt.innerText = `${data.quote}`;
+      author.innerText = `By ${data.author}`;
+    })
+    .catch((reject) => {});
+});
