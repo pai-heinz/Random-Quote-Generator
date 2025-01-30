ac75f12eb0ffd396031d4708669c17e8f4dcd72f
phkyww <paingheink2003@gmail.com>
2025-01-30 23:18:16 +0630

    add share function and some modifications

--- assets/app.js
+++ assets/app.js
@@ -2,8 +2,9 @@ let btn = document.querySelector(".btn");
 let quote_txt = document.querySelector(".quote-txt");
 let author = document.querySelector(".author");
 
+//fetch data from api and add text and author name
 btn.addEventListener("click", () => {
-  btn.innerHTML = "Loading Quote..";
+  btn.innerHTML = "Loading..";
   btn.classList.add("loading");
   fetch("https://dummyjson.com/quotes/random")
     .then((res) => {
@@ -11,13 +12,14 @@ btn.addEventListener("click", () => {
     })
     .then((data) => {
       btn.classList.remove("loading");
-      btn.innerHTML = "New Quote";
+      btn.innerHTML = "Generate";
       quote_txt.innerText = `${data.quote}`;
       author.innerText = `By ${data.author}`;
     })
     .catch((reject) => {});
 });
 
+//speech function
 let speech = document.getElementById("speech");
 speech.addEventListener("click", (_) => {
   let txtToSpeech = new SpeechSynthesisUtterance(
@@ -26,7 +28,16 @@ speech.addEventListener("click", (_) => {
   speechSynthesis.speak(txtToSpeech);
 });
 
+//copy function
 let copy = document.getElementById("copy");
 copy.addEventListener("click", (_) => {
   navigator.clipboard.writeText(quote_txt.innerText);
+  alert("Text copied!!");
+});
+
+//share function
+let x_share = document.getElementById("x-share");
+x_share.addEventListener("click", (_) => {
+  let url = `https://x.com/compose/post?url=${quote_txt.innerText}`;
+  window.open(url, "_blank");
 });
